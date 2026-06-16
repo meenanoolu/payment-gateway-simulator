a34212c87cefe8db6a4a371f4e858d37638d08b8
Meena <meenanoolu@gmail.com>
2026-06-16 21:57:55 +0530

    Initial FastAPI setup

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message": "Payment Gateway Simulator"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "running"
+    }
